6382ed369866d12892d24893ebb469354b5b50ce
Sasha <aleksandrpaprika@gmail.com>
2025-02-24 16:30:58 +0400

    created methods for run Vehicle tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,37 @@
 package org.example;
 
 import org.example.IfElse.IfElseTasks;
+import org.example.Overwrite.SubClassefOfVehicle.Airplane;
+import org.example.Overwrite.SubClassefOfVehicle.Bicycle;
+import org.example.Overwrite.SubClassefOfVehicle.Car;
+import org.example.Overwrite.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
-        IfElseTasks ifElseTasks = new IfElseTasks();
-        ifElseTasks.ifElseTask1();
-        ifElseTasks.ifElseTask2();
-        ifElseTasks.ifElseTask3();
+        //1
+        //IfElseTasks ifElseTasks = new IfElseTasks();
+        //ifElseTasks.ifElseTask1();
+        //ifElseTasks.ifElseTask2();
+        //ifElseTasks.ifElseTask3();
+
+        //2
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Airplane airplane = new Airplane();
+
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = car;
+        vehicles[1] = bicycle;
+        vehicles[2] = airplane;
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.move();
+        }
+
+
+
+
+
     }
 }
 
