c889b07359e914d4369a34836e06a41a6268fd7d
Sasha <aleksandrpaprika@gmail.com>
2025-02-24 15:46:07 +0400

    created methods for run ifElse tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.IfElse.IfElseTasks;
+
 public class Main {
     public static void main(String[] args) {
+        IfElseTasks ifElseTasks = new IfElseTasks();
+        ifElseTasks.ifElseTask1();
+        ifElseTasks.ifElseTask2();
+        ifElseTasks.ifElseTask3();
     }
 }
 
