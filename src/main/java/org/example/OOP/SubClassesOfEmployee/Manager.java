c7681f4205b16a4684c67336d4bb3c34d4a0de53
Sasha <aleksandrpaprika@gmail.com>
2025-04-02 14:54:24 +0400

    have been added constructor

--- src/main/java/org/example/OOP/SubClassesOfEmployee/Manager.java
+++ src/main/java/org/example/OOP/SubClassesOfEmployee/Manager.java
@@ -5,7 +5,13 @@ import org.example.OOP.Employee;
 public class Manager extends Employee {
     public double bonus;
 
-    @Override public double calculateSalary() {
+    public Manager(String name, double baseSalary, double bonus) {
+        super(name, baseSalary);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calculateSalary() {
         return baseSalary + bonus;
     }
 }
