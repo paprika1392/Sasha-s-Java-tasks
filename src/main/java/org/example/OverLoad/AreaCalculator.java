6cca9afb85a8b6cdf04bbb3ff5e40708413e7b4b
Sasha <aleksandrpaprika@gmail.com>
2025-04-21 16:33:17 +0400

    corrected names of variables(s1 s2 s3 -> square)

--- src/main/java/org/example/OverLoad/AreaCalculator.java
+++ src/main/java/org/example/OverLoad/AreaCalculator.java
@@ -3,29 +3,29 @@ package org.example.OverLoad;
 public class AreaCalculator {
 
     public static double calculateArea(double radius) { //S = П * r2
-        double s1 = Math.PI * Math.pow(radius, 2);
-        System.out.println("Square of the circle = " + s1);
-        return s1;
+        double square = Math.PI * Math.pow(radius, 2);
+        System.out.println("Square of the circle = " + square);
+        return square;
     }
 
     public static double calculateArea(double length, double width) { // S = length * width
-       double s2 = length * width;
-        System.out.println("Square of the rectangle with 2 enter parameters =  " + s2);
-        return s2;
+       double square = length * width;
+        System.out.println("Square of the rectangle with 2 enter parameters =  " + square);
+        return square;
     }
 
     public static double calculateArea(double halfOfOne, double side, double height) { //A = 1/2 × b × h
-        double s3 = 0;
+        double square = 0;
 
         if (halfOfOne == 0.5) {
-            s3 = halfOfOne * side * height;
-            System.out.println("Square of the rectangle with 3 enter parameters =  " + s3);
-            return s3;
+            square = halfOfOne * side * height;
+            System.out.println("Square of the rectangle with 3 enter parameters =  " + square);
+            return square;
         } else {
             System.out.println("The first parameter " + halfOfOne + " isn't equal 0.5. " +
                     "Pleas enter 0.5 like first parameter");
         }
-        return s3;
+        return square;
 
     }
 }
